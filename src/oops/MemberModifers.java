2abc6cc029a1080da9b8680a50e515690739734e
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 17:03:01 +0530

    Member Modifers

---
+++ src/oops/MemberModifers.java
@@ -0,0 +1,28 @@
+package oops;
+
+public class MemberModifers {
+
+	/*
+	 * Member = Method/Variable
+	 * 
+	 * 1.public - access any where - global level(make sure the corresponding class
+	 * have public modifier. if you mention default modifier we can't access method
+	 * outside of package
+	 * 
+	 * 2.default - access only with in the same package. - Package level-Outside
+	 * package we can't access
+	 * 
+	 * 3.private - access only with in the same class
+	 * 
+	 * 4.protected - access with in the current package but from outside package
+	 * only in Child Class's
+	 * 
+	 * -->(only we should use child class reference to access protected members from
+	 * parent class)
+	 * 
+	 * protected = default+ Child Classes
+	 * 
+	 * private < default < protected < public
+	 * 
+	 */
+}
