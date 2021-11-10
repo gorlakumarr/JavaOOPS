813b113768f2299937e6ffed608f008754a18acc
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 17:49:07 +0530

    Encapsulation

--- src/oops/OOPS_1_Encapuslation.java
+++ src/oops/OOPS_1_Encapuslation.java
@@ -5,5 +5,6 @@ public class OOPS_1_Encapuslation {
 	 * Process grouping data members into single unit is called Encapsulation
 	 *
 	 * Any component having : (data Hiding) +(abstraction) = Encapsulation
+	 *
 	 */
 }
