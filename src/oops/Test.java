dedad60947c91aa4fec87194feb22d700124f98c
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 12:45:40 +0530

    Java Source File Structure- Import Statement

---
+++ src/oops/Test.java
@@ -0,0 +1,12 @@
+package oops;
+
+//import java.util.ArrayList;  // Explicit Import Statement (Recommended - Readability is good)
+//or
+//import java.util.*;// Implicit Import Statement
+
+public class Test {
+	public static void main(String[] args) {
+		// Fully Qualified Name
+		java.util.ArrayList<Integer> arrayList = new java.util.ArrayList<Integer>();
+	}
+}
