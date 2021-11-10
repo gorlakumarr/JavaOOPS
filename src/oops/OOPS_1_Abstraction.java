acb6a88abb07b01a43d9d2e6afd4e11d2638b94b
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 17:43:18 +0530

    Abstracttion

---
+++ src/oops/OOPS_1_Abstraction.java
@@ -0,0 +1,15 @@
+package oops;
+
+public class OOPS_1_Abstraction {
+	/*
+	 * Hiding the internal implementation and highlight set of the services offered
+	 * 
+	 * Advantages:
+	 * 
+	 * 1.Security
+	 *
+	 * 2.Enhancement become very easy with out effecting the end user
+	 * 
+	 * 3.Maintainlabelity
+	 */
+}
