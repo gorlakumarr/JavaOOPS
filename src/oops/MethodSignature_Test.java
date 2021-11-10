fb6ec76548582f61f940e3301df711fcd6777606
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 18:44:24 +0530

    Added Types of Inheritance

---
+++ src/oops/MethodSignature_Test.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class MethodSignature_Test {
+	/*
+	 * Method Signature = Method Name + Argument(s) Type
+	 * 
+	 * While resolving method call compiler will use method signature's.
+	 * 
+	 * In class, It's not allowed 2 methods having same method signature's
+	 */
+	public void m1(int i, float j) {
+	}
+
+	public void m2(String s) {
+	}
+}
