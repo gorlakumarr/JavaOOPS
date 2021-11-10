3e3aefdc11d48a3d8d65e350987f940575f0238b
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 18:23:53 +0530

    Added Types of Inheritance

---
+++ src/oops/InheritenceExamples.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class InheritenceExamples {
+	/*
+	 * 1.Single Inheritance
+	 * 
+	 * Class B extends Class A
+	 * 
+	 * 2.Multiple Inheritance
+	 * 
+	 * Class A : Class B
+	 * 
+	 * Class C extends A and extends B
+	 * 
+	 * 3.Multilevel Inheritance
+	 * 
+	 * Class C extends B and extends A
+	 * 
+	 * 4.Hybrid Inheritance
+	 * 
+	 */
+}
