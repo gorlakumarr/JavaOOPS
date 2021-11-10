3412b77e337f3c51788dde7be1b5b7e20c046945
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 13:17:56 +0530

    Class Level Modifiers: public and default

---
+++ src/oops/ClassModifiers.java
@@ -0,0 +1,36 @@
+package oops;
+
+/*
+ * Modifiers: Behavior of the class
+ * 
+ * Top level class modifiers:
+ * 	1.public
+ * 	2.default
+ * 	3.abstract
+ * 	4.final
+ * 	5.strictfp
+ * 
+ * for Inner Class modifiers: along with above extra we need below:
+ * 
+ * 6.private
+ * 7.protected
+ * 8.static
+ * 
+ * 
+ */
+
+public class ClassModifiers {
+	// Accessible from anywhere
+}
+
+class ClassModifiers1 {
+	// Accessible from with in the package
+}
+
+abstract class ClassModifiers2 {
+	// We cann't create object
+}
+
+final class ClassModifiers3 {
+	// Child class creation is not possible
+}
