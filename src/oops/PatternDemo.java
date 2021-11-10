f13d6c674e389db6dd673ae7495d8aa9be5980d9
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-10 12:55:50 +0530

    Java Source File Structure- Import Statement

---
+++ src/oops/PatternDemo.java
@@ -0,0 +1,11 @@
+package oops;
+
+//import java.util.regex.Pattern; // Suitable one
+import java.util.regex.*;// Suitable one
+//import java.*; // Not Suitable
+
+public class PatternDemo {
+	public static void main(String[] args) {
+		Pattern pattern = Pattern.compile("a");
+	}
+}
