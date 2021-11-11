2d83b1a68a629895e3026791d5702f2a2da045a3
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-11 13:15:54 +0530

    Added Overloading Case Study

--- src/oops/OOPS_1_Overloading_Study_4.java
+++ src/oops/OOPS_1_Overloading_Study_4.java
@@ -1,20 +1,18 @@
 package oops;
 
 public class OOPS_1_Overloading_Study_4 {
-	public void m1(int i) {
-		System.out.println("int args method");
+	public void m1(int i, float f) {
+		System.out.println("int float args method");
 	}
 
-	public void m1(int... sb) {
-		System.out.println("int var args method");
+	public void m1(float f, int i) {
+		System.out.println("float int args method");
 	}
 
 	public static void main(String[] args) {
 
 		OOPS_1_Overloading_Study_4 ref = new OOPS_1_Overloading_Study_4();
-		ref.m1(0);// int args method
-		ref.m1();// int var args method
-		ref.m1(10, 12);// int var args method
-		ref.m1(10);// int args method
+		ref.m1(10, 10.5f);// int float args method
+		ref.m1(10.5f, 10);// float int args method
 	}
 }
---
+++ src/oops/OOPS_1_Overloading_Study_5.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class OOPS_1_Overloading_Study_5 {
+	public void m1(int i) {
+		System.out.println("int args method");
+	}
+
+	public void m1(int... sb) {
+		System.out.println("int var args method");
+	}
+
+	public static void main(String[] args) {
+
+		OOPS_1_Overloading_Study_5 ref = new OOPS_1_Overloading_Study_5();
+		ref.m1(0);// int args method
+		ref.m1();// int var args method
+		ref.m1(10, 12);// int var args method
+		ref.m1(10);// int args method
+	}
+}
