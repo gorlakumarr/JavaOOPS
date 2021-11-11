b717c11fe6158524ee3dd1e6b65d5de512d47f5c
Kumar <31374797+gorlakumarr@users.noreply.github.com>
2021-11-11 13:10:58 +0530

    Added Overloading Case Study

--- src/oops/OOPS_1_Overloading_Study_4.java
+++ src/oops/OOPS_1_Overloading_Study_4.java
@@ -1,18 +1,18 @@
 package oops;
 
 public class OOPS_1_Overloading_Study_4 {
-	public void m1(String s) {
-		System.out.println("String args method");
+	public void m1(int i) {
+		System.out.println("int args method");
 	}
 
-	public void m1(StringBuffer sb) {
-		System.out.println("StringBuffer args method");
+	public void m1(int... sb) {
+		System.out.println("int var args method");
 	}
 
 	public static void main(String[] args) {
 		OOPS_1_Overloading_Study_4 ref = new OOPS_1_Overloading_Study_4();
-		ref.m1("Durga");// String args method
-		ref.m1(new StringBuffer());// StringBuffer args method
-//		ref.m1(null);// The method m1(String) is ambiguous for the type OOPS_1_Overloading_Study_3
+		ref.m1(0);// int args method
+		ref.m1();// int var args method
+		ref.m1(10, 12);// int var args method
 	}
 }
